394bd6b69aeaf855fa2e0690301377a77ba89c81
Namankrsingh <namankr955@gmail.com>
2025-04-21 11:40:21 +0530

    Update

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Naman%401805@localhost:5433/fastapi_blog"       # @ change with code
+DATABASE_URL = "postgresql://postgres:naman123@localhost:5432/fastapi_blog"       # @= %40 change with code
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
